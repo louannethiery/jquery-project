086f3a07d77a144fcd14be9dcb6cfa6cc6038f22
ton pseudo <lou-annethiery@MacBook-Pro-de-Lou-Anne.local>
2019-02-13 09:40:58 +0100

    navbar fixe et reduce height

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 $(window).on("scroll",function(){
-    let positionY = $(this).scrollTop();
-    // $(window.height()) //Hauteur de la page
+  let positionY = $(this).scrollTop();
 
-    // Afficher arrowtotop une fois en bas de la page
+  // Afficher arrototop une fois en bas de la page
+  if(positionY > 600){
+      $("#scrolltotop").css("opacity", 1);
+  }
+  if(positionY < 600){
+      $("#scrolltotop").css("opacity", 0);
+  }
 
-    if(positionY > 1027){
-        $("#scrolltotop").css("opacity",1);
-    }
-    if(positionY < 100){
-        $("#scrolltotop").css("opacity",0);
-    }
-})
+  if(positionY > 100){
+    $(".nav-bar").css("padding", "0px 10px");
+    
+  }else{
+    $(".nav-bar").css("padding", "20px 10px");
+
+  }
+});
